774dda509a3699b076f1fd360bd3a3b645ba8587
kozecm <kozecm@oregonstate.edu>
2020-03-05 21:30:29 -0800

    changing db and adding user/playlist

--- Serverless/index.js
+++ Serverless/index.js
@@ -8,8 +8,10 @@ const app = express()
 const AWS = require('aws-sdk');
 
 
-const USERS_TABLE = "Users";
+const USERS_TABLE = "SLSUsers";
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const UID = '';
+
 app.use(cors());
 
 app.use(bodyParser.json({ strict: false }));
@@ -20,16 +22,16 @@ app.get('/', function (req, res) {
 
 // Get User endpoint
 app.post('/save-user',  (req, res) => {
-  var id = req.query.id;
+  UID = req.query.id;
   var name = req.query.name;
   var email = req.query.email;
 
   var params = {
     TableName: USERS_TABLE,
     Item: {
-      ID: id,
+      Primary: UID,
       name: name,
-      email: email
+      Sort: email
     },
   }
 
@@ -40,6 +42,50 @@ app.post('/save-user',  (req, res) => {
     else{
       console.log(data);
       res.sendStatus(201);
+    } 
+  });
+})
+
+app.post('user/playlist', (req, res) => {
+  var songid = req.query.songid;
+
+  var params = {
+    Item: {
+      Primary: UID,
+      Sort: songid,
+      playlist: "playlist"
+    },
+    TableName: USERS_TABLE
+  }
+
+  dynamoDb.put(params, function(err, data) {
+    if (err) res.send("ERROR\n", err);
+    else{
+      console.log(data);
+      res.sendStatus(201);
+    } 
+  });
+})
+
+app.get('user/playlist', (req,res) => {
+  var result = []
+  var params = {
+    TableName: USERS_TABLE,
+    FilterExpression: 'Primary = :uid and playlist = :play',
+    ExpressionAttributeValues: {
+      ':uid': UID,
+      ':play': "playlist"
+    }
+  };
+
+  dynamoDb.scan(params, function (err, data) {
+    if (err) console.log(err);
+    else{
+      data.Items.forEach((i) => {
+        result.push(i.SortKey);
+      });
+      // console.log("result: ", result);
+      return result;
     } 
   });
 })
